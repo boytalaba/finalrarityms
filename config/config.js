9cba6e7cac7e19c971c3d8c5330e52bd061030e9
boytalaba <36549438+boytalaba@users.noreply.github.com>
2022-10-18 00:41:29 -0700

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const config = {
+    app_name: 'MoonShibs Rarity',
+    app_description: 'MoonShibs Rarity is an open source package for easy rarity score calculation with ERC721 NFT metadata collection.',
+    collection_file_name: 'collection.json',
+    collection_contract_address: '0x660d1258c5f142c55bdafe48a02d7bb9e81eba95',
+    collection_name: 'MoonShibs',
+    collection_description: 'Moonshibs is a collection of 4,444 unique NFTs living in EthereumPow blockchain. What makes this project different than any other NFT project is that they are very unique and made details by details. Of the 4,444 Moonshibs, there are different species (colors) of shiba and different layers and the total possible combinations.We wanted to make something truly different.',
+    collection_id_from: 1,
+    ignore_traits: ['date'], 
+    sqlite_file_name: 'database.sqlite',
+    ga: 'G-BW69Z04YTP',
+    main_og_image: 'https://ibb.co/cNJmrd6',
+    item_path_name: 'MoonShibs',
+    page_item_num: 60,
+    content_image_is_video: false,
+    content_image_frame: 'circle', // circle, rectangle
+    use_wallet: false
+};
+
+module.exports = config;
